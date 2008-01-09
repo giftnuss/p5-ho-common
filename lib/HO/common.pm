2e42dd7b59f8024a93dbe8cde7cd45263325e032
sknpp_ftr <sknpp_ftr@63df29d3-4323-0410-adbf-b7f0d5065d14>
2008-01-09 09:17:17 +0000

    commonly used functions when you work with HO

---
+++ lib/HO/common.pm
@@ -0,0 +1,63 @@
+  package HO::common
+# ******************
+; our $VERSION='0.01'
+# *******************
+
+; use strict
+
+; require Exporter
+; our @ISA = ('Exporter')
+
+; our @EXPORT = ()
+; our @EXPORT_OK = qw/node newline/
+
+; use HO ()
+
+# functional constructor alias - future compatible -
+# good for example for learning by doing - it is not a good idea
+# to give it the same name like the class
+
+; sub node { new HO(@_) }
+
+; sub newline() { node("\n") }
+
+; 1
+
+__END__
+
+=head1 NAME
+
+HO::common
+
+=head1 SYNOPSIS
+
+  use HO::common qw/node newline/;
+  
+  my $n = node() << newline();
+  
+=head1 DESCRIPTION
+
+This module contains some functions which are useful when you work
+with hierarchical object (HO) modules.
+
+=head2 node()
+
+This is an alias to say C<new HO()>. The sense why to this in favour of the
+direct constructor call is that a fuction is easier replaced during runtime.
+
+=head2 newline()
+
+This only a function to say C<"\n"> which is used for the same reason.
+
+=head1 AUTHOR
+
+Sebastian Knapp E<lt>sk@computer-leipzig.comE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007-2008 Sebastian Knapp, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
