e81bd04cb44c1e970d11805c4ca759cb30c5e0d0
Sebastian Knapp <sknapp@cl-test1.sterne>
2010-02-03 13:54:10 +0100

    simple is better - bug with class and style solved

--- lib/HO/HTML/attribute.pm
+++ lib/HO/HTML/attribute.pm
@@ -6,8 +6,8 @@
 ; sub style
     { my ($self,@args) = @_
     ; if(@args)
-        { (my $style = $self->get_attribute('style'))
-            and unshift @args,$style
+        { my $style = $self->get_attribute('style')
+        ; unshift @args,$style if $style
         ; return $self->set_attribute('style',join(";",@args))
         }
       else
@@ -18,8 +18,8 @@
 ; sub class
     { my ($self,@args) = @_
     ; if(@args)
-        { (my $class = $self->get_attribute('class'))
-            and unshift @args,$class
+        { my $class = $self->get_attribute('class')
+        ; unshift @args,$class if $class
         ; return $self->set_attribute('class',join(" ",@args))
         }
       else
--- t/027_html_functional.t
+++ t/027_html_functional.t
@@ -1,5 +1,5 @@
 use strict; use warnings;
-use Test::More tests => 3;
+use Test::More tests => 4;
 
 use HO::HTML functional => 1;
 
@@ -8,3 +8,4 @@ is_deeply([HO::HTML::->list_loaded],[HO::HTML::->list_names]);
 is(A('MFG')."",'<a>MFG</a>');
 is(Img()->src('./test.jpg')."",'<img src="./test.jpg" >');
 
+is(Div('abc')->class('holly')->class('wood')."",'<div class="holly wood">abc</div>');
