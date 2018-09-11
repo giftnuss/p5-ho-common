572afc53bf1233b3514ff0ff92329355e0a455a7
Sebastian B. Knapp <news@young-workers.de>
2018-09-11 16:01:41 +0200

    s/common/Common/

--- lib/HO/XHTML/Style.pm
+++ lib/HO/XHTML/Style.pm
@@ -1,12 +1,12 @@
   package HO::XHTML::Style
 # ************************
-; our $VERSION = '0.01'
+; our $VERSION = '0.02'
 # *********************
 ; use strict; use warnings; use utf8
 
 ; use parent 'HO::HTML::Style'
 
-; use HO::common qw/newline/
+; use HO::Common qw/newline/
 
 ; sub Style
     { HO::HTML::Style('/* <![CDATA[ */',newline(@_),'/* ]]> */')->type('text/css')
